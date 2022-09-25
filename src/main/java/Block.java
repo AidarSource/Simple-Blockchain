7b55f525767c54f0cf8838ff92fa77c3346ea2b7
Switchback <aidartemirgaliev@gmail.com>
2022-09-25 22:19:50 +0600

    add mining feature

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -5,6 +5,7 @@ public class Block {
     public String prevHash;
     private String data;
     private long timestamp;
+    private int nonce;
 
     public Block( String prevHash, String data ) {
         this.prevHash = prevHash;
@@ -15,9 +16,18 @@ public class Block {
 
     public String calculateHash() {
         String calculatedHash = StringUtil.applySha256(
-                prevHash + Long.toString( timestamp ) + data
+                prevHash + Long.toString( timestamp ) + Integer.toString( nonce ) + data
         );
 
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring( 0, difficulty ).equals( target )) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block mined! : " + hash);
     }
 }
