cabf23acb855cc24e43535824573998716206404
Switchback <aidartemirgaliev@gmail.com>
2022-09-28 00:25:25 +0600

    methods for applying and verifying ECDSASig, and getStringFromKey method

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -1,5 +1,6 @@
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
+import java.security.*;
+import java.util.Base64;
 
 public class StringUtil {
     public static String applySha256(String input) {
@@ -17,5 +18,39 @@ public class StringUtil {
         } catch( Exception e ) {
             throw new RuntimeException(e);
         }
+    }
+
+    // Applies ECDSA Signature and returns the result (as bytes)
+    public static byte[] applyECDSASig( PrivateKey privateKey, String input ) {
+        Signature dsa;
+        byte[] output = new byte[0];
+        try {
+            dsa = Signature.getInstance( "ECDSA", "BC" );
+            dsa.initSign( privateKey );
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSig = dsa.sign();
+            output = realSig;
+        } catch( Exception e ) {
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+
+    // Verifies a String signature
+    public static boolean verifyECDSASig( PublicKey publicKey, String data, byte[] signature ) {
+        try {
+            Signature ecdsaVerify = Signature.getInstance( "ECDSA", "BC" );
+            ecdsaVerify.initVerify( publicKey );
+            ecdsaVerify.update( data.getBytes() );
+            return ecdsaVerify.verify( signature );
+        } catch( Exception e ) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getStringFromKey( Key key) {
+        return Base64.getEncoder().encodeToString( key.getEncoded() );
     }
 }
