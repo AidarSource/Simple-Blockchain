0d2b0bf69f3728e1eab99ca36be2cfcc79d50d99
Switchback <aidartemirgaliev@gmail.com>
2022-09-27 23:32:27 +0600

    add bouncycastle dependency

---
+++ src/main/java/Wallet.java
@@ -0,0 +1,26 @@
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance( "ECDSA", "BC" );
+            SecureRandom random = SecureRandom.getInstance( "SHA1PRNG" );
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec( "prime192v1" );
+
+            // Initialize the key generator and generate KeyPair
+            keyGen.initialize( ecSpec, random ); // 256 bytes provides an acceptable security level
+            KeyPair keyPair = keyGen.generateKeyPair();
+
+            // Set the public and private keys from the keyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
