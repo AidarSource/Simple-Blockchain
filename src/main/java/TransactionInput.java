4474ebc379002393cec1896d58f5d487fcd1ac98
Switchback <aidartemirgaliev@gmail.com>
2022-10-07 19:11:50 +0600

    reference TransactionOutput that not yet been spent

--- src/main/java/TransactionInput.java
+++ src/main/java/TransactionInput.java
@@ -1,2 +1,9 @@
+
 public class TransactionInput {
+    public String transactionOutputId; // Reference to TransactionOutputs -> transactionId
+    public TransactionOutput UTXO; // Contains unspent transaction output
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
 }
